f4fe7cda4b9fd6c452d22d2abcd8088d79056f0d
Jacob Delott <jldelott@gmail.com>
2025-03-21 09:30:40 -0700

    another commit 11

---
+++ bot-server.js
@@ -0,0 +1,13 @@
+const { startBot } = require("./src/bot");
+
+// Start the bot
+startBot().catch((error) => {
+  console.error("Failed to start bot:", error);
+  process.exit(1);
+});
+
+// Handle process termination
+process.on("SIGINT", () => {
+  console.log("Bot shutting down...");
+  process.exit(0);
+});
