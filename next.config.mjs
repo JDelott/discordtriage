9332d692c37410a2054055ac3615c6bb50e0515b
Jacob Delott <jldelott@gmail.com>
2025-03-20 20:54:31 -0700

    more edits to deploy 2

--- next.config.mjs
+++ next.config.mjs
@@ -3,25 +3,16 @@ const nextConfig = {
   output: "standalone",
   reactStrictMode: true,
   webpack: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
       bufferutil: "commonjs bufferutil",
       encoding: "commonjs encoding",
       "zlib-sync": "commonjs zlib-sync",
     });
-
-    // Add rule for native modules
-    config.module.rules.push({
-      test: /\.node$/,
-      use: "node-loader",
-      type: "javascript/auto",
-    });
-
     return config;
+  },
+  experimental: {
+    outputFileTracingRoot: undefined,
   },
 };
 
