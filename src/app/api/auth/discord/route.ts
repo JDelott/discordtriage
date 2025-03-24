844b328f0669686b2786d075b1a5d34076ce259f
Jacob Delott <jldelott@gmail.com>
2025-03-24 11:16:33 -0700

    auth fix 22

--- src/app/api/auth/discord/route.ts
+++ src/app/api/auth/discord/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { BOT_CONFIG } from '../../../../bot/config';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const guildId = searchParams.get('guild');
